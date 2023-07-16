aa6775ab9764efe887768dd8ec6978758e0ed958
Anton Matushkin <matushkin-2017@mail.ru>
2023-07-16 18:56:43 +0400

    Project was restructured.

---
+++ src/main/java/Main.java
@@ -0,0 +1,144 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dotenv dotenv = null;
+        String username = "admin";
+        String password = "admin";
+        String connectionUrl = "jdbc:mysql://localhost:3306/school_management";
+
+        try{
+            Connection connection = DriverManager.getConnection(connectionUrl, username, password);
+            System.out.println("we're connected!");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+
+        School school = new School();
+        new Teacher();
+        new Student();
+        Scanner scanner = new Scanner(System.in);
+        ConsoleColors consoleColors = new ConsoleColors();
+        int teacherId = 0;
+        int studentId = 0;
+
+
+        info();
+        String response = "";
+        while (!response.equals("Q")){
+
+            response = scanner.nextLine();
+            response = response.toUpperCase().replaceAll("\\s", "");
+
+            switch (response) {
+                case ("TEACH") -> {
+                    school.addTeacher();
+                    ArrayList<Teacher> list_of_teachers = school.getList_of_teachers();
+                    if (list_of_teachers != null && !list_of_teachers.isEmpty()) {
+                        Teacher teacher = list_of_teachers.get(list_of_teachers.size() - 1);
+
+                        teacherId++;
+                        teacher.setId(teacherId);
+
+                        System.out.println("Enter the teacher's name: ");
+                        teacher.enterName();
+
+                        System.out.println("Enter the teacher's gender: ");
+                        System.out.println("f - Female, m - Male");
+                        teacher.enterGender();
+
+                        System.out.println("Enter the teacher's age");
+                        System.out.println("(the age can be from 17 to 120 inclusive):");
+                        teacher.enterAge(17, 120);
+
+                        System.out.println("Enter the teacher's position: ");
+                        teacher.enterPost();
+
+                        System.out.println("Enter the teacher's salary: ");
+                        teacher.enterSalary();
+
+                        System.out.println("Enter the teacher's experience: ");
+                        teacher.enterExperience();
+
+                        consoleColors.GREEN_BOLD("Done");
+                    }
+                }
+                case ("STUD") -> {
+                    school.addStudent();
+                    ArrayList<Student> list_of_students = school.getList_of_students();
+                    if (list_of_students != null && !list_of_students.isEmpty()){
+                        Student student = list_of_students.get(list_of_students.size() - 1);
+                        studentId++;
+                        student.setId(studentId);
+
+                        System.out.println("Enter the student's name: ");
+                        student.enterName();
+
+                        System.out.println("Enter the student's gender: ");
+                        System.out.println("f - Female, m - Male");
+                        student.enterGender();
+
+                        System.out.println("Enter the student's age: ");
+                        student.enterAge(6, 20);
+
+                        System.out.println("Enter the student's grade: ");
+                        student.enterGrade();
+
+                        System.out.println("Enter the student's fees: ");
+                        student.enterFees();
+
+                        consoleColors.GREEN_BOLD("Done");
+                    }
+                }
+                case ("TDATA") -> {
+
+                    if (school.getList_of_teachers() != null && !school.getList_of_teachers().isEmpty()){
+                        school.getData("Teacher");
+                    }
+                    else{
+                        consoleColors.YELLOW("It's seems like there are no teachers.");
+                    }
+
+                }
+                case ("SDATA") -> {
+                    if (school.getList_of_students() != null && !school.getList_of_students().isEmpty()){
+                        school.getData("Student");
+                    }
+                    else {
+                        consoleColors.YELLOW("It's seems like there are no students.");
+                    }
+                }
+                case ("HELP") -> info();
+
+                case ("Q") -> consoleColors.GREEN_BOLD("Goodbye!");
+                case (""), (" "), ("\n") -> {}
+                default -> consoleColors.RED("Not a valid response.");
+
+            }
+        }
+
+    }
+    public static void info(){
+        System.out.println();
+        System.out.println("-------------------------");
+        System.out.println("TEACH - add new teacher");
+        System.out.println("STUD - add new student");
+        System.out.println("TDATA - get teachers data");
+        System.out.println("SDATA - get students data");
+        System.out.println("HELP - shows this menu again");
+        System.out.println("Q - quit");
+        System.out.println("-------------------------");
+    }
+
+
+}
+// TODO: delete color things for console, 'cause it's doesn't work for standard windows one.
+
